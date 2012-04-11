20c3ec2ef86e920b3d9d5b6106216f4817c1deb7
gwan284@gmail.com <gwan284@gmail.com@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-04-11 13:49:23 +0000

    MailSender class

---
+++ trunk/Week1/MailSender/MailSender.java
@@ -0,0 +1,64 @@
+package mailer;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Properties;
+
+public class MailSender {
+
+    static final Properties conf = new Properties();
+
+    static {
+        try {
+            conf.load(new FileInputStream("config.properties"));
+        } catch (IOException ex) {
+            System.err.println("Check configuration file");
+        }
+    }
+    static final String SMTP_OK_CODE = conf.getProperty("SMTPOkCode");
+
+    static public boolean send(String from, String to, String subject, String data) {
+        
+        final int SMTP_PORT = Integer.parseInt(conf.getProperty("port"));
+        final String localhostName = conf.getProperty("localhostName");
+        final String mailhost = conf.getProperty("mailhost");
+
+        try (Socket smtpPipe = new Socket(mailhost, SMTP_PORT)) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(smtpPipe.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                    new OutputStreamWriter(smtpPipe.getOutputStream()), true);
+            in.readLine();
+            out.println("HELO " + localhostName);
+            if (!okReturned(in.readLine())) {
+                return false;
+            }
+            out.println("MAIL FROM:<" + from + ">");
+            if (!okReturned(in.readLine())) {
+                return false;
+            }
+            out.println("RCPT TO:<" + to + ">");
+            if (!okReturned(in.readLine())) {
+                return false;
+            }
+            out.println("DATA");
+            out.println("Subject:" + subject);
+            out.println(data);
+            out.println(".");
+            in.readLine();
+            if (!okReturned(in.readLine())) {
+                return false;
+            }
+            out.println("QUIT");
+            
+        } catch (IOException ex) {
+            System.err.println("Unable to connect to the SMTP server");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean okReturned(String answer) {
+        return answer.contains(SMTP_OK_CODE);
+    }
+}
