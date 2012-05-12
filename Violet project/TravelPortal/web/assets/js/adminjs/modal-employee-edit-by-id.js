6ad0c497f978ce117c9cebae1fe59a7ccd71388f
jiallombardo <jiallombardo@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-05-12 08:56:18 +0000

    Adapted scripts for handling the tree

--- Violet project/TravelPortal/web/assets/js/adminjs/modal-employee-edit-by-id.js
+++ Violet project/TravelPortal/web/assets/js/adminjs/modal-employee-edit-by-id.js
@@ -5,8 +5,53 @@
  */
 
 $(document).ready(function(){
-    checkEmployeeId() 
+    checkEmployeeId();
+    var depEditTitle = 'Edit department <a class="close" onclick=\'$("#pop1").popover("toggle")\'>&times;</a>';
+    var depEdit = 'Department name <input type="text" id="depEdit" value=""> <br>' +
+        '<input type="button" id="ediButton" class="btn" value="Edit" onclick="if (confirm(\'Confirm new department?\')) { newDep(null) }">'
+    $('#pop1').attr("data-content", depEdit);
+    $('#pop1').attr("title", depEditTitle);
+    //    $('#pop1').position = relative;
+    
+    $('#pop1').popover({
+        trigger : 'manual', 
+        title : depEditTitle, 
+        content : depEdit,
+        placement: whereToPlace
+    }); 
+    var depNewTitle = 'New department <a class="close" onclick=\'$("#pop2").popover("toggle")\'>&times;</a>';
+    var depNew = 'Department name <input type="text" id="depNew" value=""> <br>' +
+        '<input type="button" id="newButton" class="btn" value="New" onclick="if (confirm(\'Confirm edit?\')) { newDep(null) }">'
+    $('#pop2').attr("data-content", depNew);
+    $('#pop2').attr("title", depNewTitle);
+    //    $('#pop1').position = relative;
+    
+    $('#pop2').popover({
+        trigger : 'manual', 
+        title : depEditTitle, 
+        content : depEdit,
+        placement: whereToPlace
+    }); 
 });
+
+function whereToPlace() {
+    var myLeft = $(this).offset.left;
+    
+    if (myLeft < 500) return 'bottom';
+    return 'bottom';
+}
+function alterPop() {
+    if ($('#tree').jqGrid('getGridParam', 'selrow') == null) {
+        return;
+    }
+    else {
+        var id = $('#tree').jqGrid('getGridParam', 'selrow');
+        var nm = ($('#tree').getRowData(id, true))['name'];
+        $('#depEdit').attr('value', nm);
+        $('#ediButton').attr('onclick', 'if (confirm(\'Confirm edit?\')) { editDep('+id+')}');
+        $('#newButton').attr('onclick', 'if (confirm(\'Confirm new department?\')) { newDep('+id+')}');
+    }
+};
     
 $(window).bind('hashchange', function() {
     //window.location.reload();
@@ -14,113 +59,157 @@ $(window).bind('hashchange', function() {
     checkEmployeeId()
 });
 
+function newDep(id) {
+
+    if (id  == null) {
+        $.getJSON(getContextPath() + 
+            "/departmentaugmenthandler?action=new&depNew=" + $('#depNew').val()+
+            "&level=0",
+        function(data) {
+            //            alert(data['jsdata']);
+            $('#tree').jqGrid('addChildNode', data['nodeid'], id, data['jsdata']);
+            $("#pop2").popover("toggle");
+        });
+    }
+    else {
+        $.getJSON(getContextPath() + 
+            "/departmentaugmenthandler?depid="+id+"&action=new&depNew="
+            + $('#depNew').val()+ "&level="+(($('#tree').getRowData(id, true)).level + 1),
+        function(data) {
+            //            alert(data['jsdata']);
+            $('#tree').jqGrid('addChildNode',data['nodeid'],id, data['jsdata']);
+            $("#pop2").popover("toggle");
+        });
+    } 
+}
+
+
+function editDep(id) {
+if (id == null) {
+    return;
+}
+else {
+    $.getJSON(getContextPath() + 
+        "/departmentaugmenthandler?depid="+id+"&action=edit&depEdit=" + $('#depEdit').val(),
+    function(data) {
+        //            alert(data['jsdata']);
+        $('#tree').jqGrid('setTreeRow',id, data['jsdata']);
+        $("#pop1").popover("toggle");
+    });
+}
+}
 
 function checkEmployeeId(){
-    var q = window.location;
-    var reg=/.*#(.*)/  
-    var arr=reg.exec(q)
-    if ((arr != null) && (arr.length >1)){
-        var id = arr[1];
-        var idInt = parseInt(id);
-        if (id == idInt){
-            //ajax request here
-            getEmployeeUsingAJAX(id);
-        } 
-    }
+var q = window.location;
+var reg=/.*#(.*)/  
+var arr=reg.exec(q)
+if ((arr != null) && (arr.length >1)){
+    var id = arr[1];
+    var idInt = parseInt(id);
+    if (id == idInt){
+        //ajax request here
+        getEmployeeUsingAJAX(id);
+    } 
+}
 }
 
 function getEmployeeUsingAJAX(id){
-    $('#editEmployeeModal').modal('show');
-    $.getJSON(getContextPath() + "/ajaxemployeeshandle?id="+id,
-        function (data){
-            fillEmployeeForm(data, id);
-            if(id>0){
-                $('#modalTitle').text('Employee editing');
-                $('#employeeCommit').text('Save changes');
-                $('#password').removeAttr("checked")
-            }
-            else{
-                $('#modalTitle').text('Employee creating');
-                $('#employeeCommit').text('Create employee');
-                $('#password').attr("checked","true")
-            }
-        });
+$('#editEmployeeModal').modal('show');
+$.getJSON(getContextPath() + "/ajaxemployeeshandle?id="+id,
+function (data){
+    fillEmployeeForm(data, id);
+    if(id>0){
+        $('#modalTitle').text('Employee editing');
+        $('#employeeCommit').text('Save changes');
+        $('#password').removeAttr("checked")
+    }
+    else{
+        $('#modalTitle').text('Employee creating');
+        $('#employeeCommit').text('Create employee');
+        $('#password').attr("checked","true")
+    }
+});
     
 }
 
 function fillEmployeeForm(data, id){
-    if(id>0){
-        $('#firstName').val(data['firstName']);
-        $('#lastName').val(data['lastName']);
-        prepareComboBox($("#position"), data['positions'], data['positionId']);
-        prepareComboBox($("#office"), data['offices'], data['officeId']);
-        prepareComboBox($("#department"), data['departments'], data['departmentId']);
-        $('#email').val(data['email']);
-        $('#login').val(data['login']);
-    }
-    else{
-        $('#firstName').val('');
-        $('#lastName').val('');
-        prepareComboBox($("#position"), data['positions'], 1);
-        prepareComboBox($("#office"), data['offices'], 1);
-        prepareComboBox($("#department"), data['departments'], 1);
-        $('#email').val(data['']);
-        $('#login').val(data['']);
-    }
+var string ="";
+$.each(data, function(key, value) { 
+    string += '[' + key + ',' + value + '] ';
+});
+//alert(string);
+if(id>0){
+    $('#firstName').val(data['firstName']);
+    $('#lastName').val(data['lastName']);
+    prepareComboBox($("#position"), data['positions'], data['positionId']);
+    prepareComboBox($("#office"), data['offices'], data['officeId']);
+    prepareComboBox($("#department"), data['departments'], data['departmentId']);
+    $('#email').val(data['email']);
+    $('#login').val(data['login']);
+}
+else{
+    $('#firstName').val('');
+    $('#lastName').val('');
+    prepareComboBox($("#position"), data['positions'], 1);
+    prepareComboBox($("#office"), data['offices'], 1);
+    prepareComboBox($("#department"), data['departments'], 1);
+    $('#email').val(data['']);
+    $('#login').val(data['']);
+}
 }
 
 function prepareComboBox(combobox, data, id){
-    combobox.empty();
-    $.each(data, function(key, value) { 
-        var selected;
-        if (key == id) {
-            selected = 'selected="selected"';
-        }
-        combobox.prepend( $('<option ' + selected + ' sysid="' + key + '">'+ value + '</option>'))
-    });
+combobox.empty();
+$.each(data, function(key, value) { 
+    var selected;
+    if (key == id) {
+        selected = 'selected="selected"';
+    }
+    combobox.prepend( $('<option ' + selected + ' sysid="' + key + '">'+ value + '</option>'))
+});
 }
 
 function processEmployee(){
-    var resultMap = [
-        {
-            'firstName': $('#firstName').val()
-        }, 
-        {
-            'lastName':$('#lastName').val()
-        },
-        {
-            'positionId':$("#position option:selected").attr("sysId")
-        }, 
-        {
-            'officeId':$("#office option:selected").attr("sysId")
-        },
-        {
-            'departmentId':$("#department option:selected").attr("sysId")
-        },
-        {
-            'email':$('#email').val()
-        }, 
-        {
-            'login':$('#login').val()
-        },
-        {
-            'password':($('#password').attr("checked")!=undefined).toString()
-        }
+var resultMap = [
+    {
+        'firstName': $('#firstName').val()
+    }, 
+    {
+        'lastName':$('#lastName').val()
+    },
+    {
+        'positionId':$("#position option:selected").attr("sysId")
+    }, 
+    {
+        'officeId':$("#office option:selected").attr("sysId")
+    },
+    {
+        'departmentId':$("#department option:selected").attr("sysId")
+    },
+    {
+        'email':$('#email').val()
+    }, 
+    {
+        'login':$('#login').val()
+    },
+    {
+        'password':($('#password').attr("checked")!=undefined).toString()
+    }
 
-        ];
-        $.ajax({
-        url: getContextPath() + "/ajaxemployeeprocess",
-        type: "POST",
-        data: {
-            "ajaxdata" : JSON.stringify(resultMap)
-            },
-        dataType: "json",
-        success: function(result) {
-            $('#editEmployeeModal').modal('hide')
-            $("#emptable").trigger("reloadGrid") 
-        }
+];
+$.ajax({
+    url: getContextPath() + "/ajaxemployeeprocess",
+    type: "POST",
+    data: {
+        "ajaxdata" : JSON.stringify(resultMap)
+    },
+    dataType: "json",
+    success: function(result) {
+        $('#editEmployeeModal').modal('hide')
+        $("#emptable").trigger("reloadGrid") 
+    }
         
-    });
+});
 }
 
 
