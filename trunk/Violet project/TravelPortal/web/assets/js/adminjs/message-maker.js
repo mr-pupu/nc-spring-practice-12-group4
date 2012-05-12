6f20bba1b5481c490aef6d351bb269d916052399
jiallombardo <jiallombardo@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-05-12 13:13:02 +0000

    Script for adding messages

---
+++ trunk/Violet project/TravelPortal/web/assets/js/adminjs/message-maker.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function addMessage(data){
+    $('#notifier').empty();
+    var type = data['error'];
+    var message = data['success'];
+    var messageHolder = '<center><div class=\"span4\"><div class=\"alert alert-' + type + '\">\n\
+<a class=\"close\" data-dismiss=\"alert\" onclick=\"$(\'notifier\').text(\'\');\">&times;</a>' + message+ '</div></div></center>';
+    $('#notifier').append(messageHolder);
+}
