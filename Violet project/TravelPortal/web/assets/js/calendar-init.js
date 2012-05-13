636e96ec084ef294864dca3af333dba5ab339ef6
merlev.inbox@gmail.com <merlev.inbox@gmail.com@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-05-13 17:05:21 +0000

    calendar fix

--- Violet project/TravelPortal/web/assets/js/calendar-init.js
+++ Violet project/TravelPortal/web/assets/js/calendar-init.js
@@ -1,13 +1,12 @@
-$(document).ready(calendarsInit() );
-function calendarsInit(){
+window.onload = function(){
      $('div[rel="calendarDiv"]').each(function (index)
      {  
          var id = $(this).find("input").attr('id');
+         $(this).find("span").attr('onclick', 'document.getElementById("' + id + '").focus();');
          new JsDatePick({
                 useMode:2,
                 target:id,
                 dateFormat:"%d-%m-%Y"
          });
-         $(this).find("span").attr('onclick', 'document.getElementById("' + id + '").focus();');
      });
 }
