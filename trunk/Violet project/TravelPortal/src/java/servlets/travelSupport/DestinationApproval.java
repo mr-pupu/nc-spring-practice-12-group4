781a697b93a97e63acc5efe3623a404f938eb856
jiallombardo <jiallombardo@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-05-15 20:25:38 +0000

    Servlets that handle destination approval and disapproval

---
+++ trunk/Violet project/TravelPortal/src/java/servlets/travelSupport/DestinationApproval.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets.travelSupport;
+
+import database.mapping.Destination;
+import database.utilities.HibernateUtil;
+import database.utilities.TravelSupportDesktop;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import servlets.ajax.AJAXSendHandler;
+
+/**
+ *
+ * @author Gangbang34 
+ */
+public class DestinationApproval extends AJAXSendHandler {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public DestinationApproval() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        handle(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        handle(request, response);
+    }
+
+    private void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+        //response.sendRedirect("index.jsp");
+        System.out.println("DestinationApproval runned");
+        String pageString = request.getParameter("page");
+        System.out.println("Page:" + pageString);
+        String recordString = request.getParameter("rows");
+        System.out.println("Records " + recordString);
+        int page = Integer.parseInt(pageString);
+        int rows = Integer.parseInt(recordString);
+        long count = TravelSupportDesktop.CountNoneApprovedDestinations().longValue();
+        if ((page - 1) * rows > count) {
+            page = 1;
+        }
+        JSONObject resp = new JSONObject();
+        resp.put("records", count);
+        resp.put("total", ((count/rows) + ((count%rows > 0) ? 1 : 0)));
+        resp.put("page", page);
+        JSONArray ja = new JSONArray();
+        //get paged list, process it
+        List<Destination> dlist = TravelSupportDesktop.NonApprovedDestinationListPaged(page, rows);
+        Session s = HibernateUtil.getSession();
+        for (Destination d: dlist) {
+            s.refresh(d);
+            JSONObject jo = new JSONObject();
+            JSONArray jaj = new JSONArray();
+            jo.put("id", d.getId());
+            jaj.add(d.getHotelname());
+            jaj.add(d.getHotelsite());
+            jaj.add(d.getCity().getCityName());
+            jaj.add(d.getCity().getCountry().getCountryName());
+            jaj.add("<a href='#' onclick=\"approver("+d.getId()+", true)\">"
+                    + "<i class=\"icon-ok\"></i></a>");
+            jaj.add("<a href='#' onclick=\"approver("+d.getId()+", false)\">"
+                    + "<i class=\"icon-remove\"></i></a>");
+            jo.put("cell", jaj);
+            ja.add(jo);
+        }
+        resp.put("rows", ja);
+        System.out.println(resp);
+        response.setContentType("application/json");
+        resp.writeJSONString(response.getWriter());
+    }
+}
---
+++ trunk/Violet project/TravelPortal/src/java/servlets/travelSupport/DestinationApprover.java
@@ -0,0 +1,132 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets.travelSupport;
+
+import database.mapping.Destination;
+import database.mapping.Trf;
+import database.mapping.Trfstate;
+import database.utilities.HibernateUtil;
+import java.io.IOException;
+import java.util.Set;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Master
+ */
+public class DestinationApprover extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Ran DestinationApprover");
+        String idstr = request.getParameter("id");
+        String approve = request.getParameter("approve");
+        System.out.println(idstr + " " + approve);
+        JSONObject json = new JSONObject();
+        Session s = HibernateUtil.getSession();
+        Transaction tx = s.beginTransaction();
+        try {
+            Boolean app = Boolean.valueOf(approve);
+            System.out.println(app);
+            Long id = Long.valueOf(idstr);
+            //approving destination
+            if (app) {
+                System.out.println("COCK");
+                Destination des = (Destination) s.get(Destination.class, id.longValue());
+                des.setIsApproved(true);
+                s.save(des);
+                tx.commit();
+                json.put("error", "success");
+                json.put("success", "Destination approved");
+                json.writeJSONString(response.getWriter());
+            }
+            //delete destination
+            //set all associated trfs to rejected state
+            //comment that the destination is wrong
+            else {
+                Destination des = (Destination) s.get(Destination.class, id.longValue());
+                Set<Trf> tset = des.getTrfs();
+                for (Trf tr : tset) {
+                    tr.setCurState((short)1);
+                    s.save(tr);
+                    Trfstate tstate = new Trfstate();
+                    tstate.setChanger(Long.parseLong(request.getSession().getAttribute("userId").toString()));
+                    tstate.setTrf(tr);
+                    tstate.setStatus((short)1);
+                    tstate.setCommentary("Please choose another destination");
+                    s.save(tstate);
+                }
+                s.delete(des);
+                tx.commit();
+                json.put("error", "success");
+                json.put("success", "Destination deleted");
+                json.writeJSONString(response.getWriter());
+            }
+        }
+        catch (Exception e) {
+            tx.rollback();
+            json.put("error", "error");
+            json.put("success", e.getMessage());
+            json.writeJSONString(response.getWriter());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
