271fb49d4340c9b544994109c8d282b3d6f80a2b
gwan284 <gwan284@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-05-16 10:39:18 +0000

    little changes in mail sender class

--- Violet project/TravelPortal/src/java/utils/MailSender.java
+++ Violet project/TravelPortal/src/java/utils/MailSender.java
@@ -2,60 +2,67 @@ package utils;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 import java.util.Properties;
 
 public class MailSender {
 
-    static final Properties conf = new Properties();
-
+    static final Properties serverConfig = new Properties();
+        
     static {
         try {
-            conf.load(new FileInputStream("config.properties"));
+            serverConfig.load(new FileInputStream("config.properties"));
         } catch (IOException ex) {
-            System.err.println("Check configuration file");
         }
     }
-    static final String SMTP_OK_CODE = conf.getProperty("SMTPOkCode");
+    
+    final static String localhostName = serverConfig.getProperty("localhostName");
+    final static String mailhost = serverConfig.getProperty("mailhost");
+    final static String sender = serverConfig.getProperty("noreply");
+    final static String SMTP_OK_CODE = serverConfig.getProperty("SMTPOkCode");
+    final static int SMTP_PORT = Integer.parseInt(serverConfig.getProperty("port"));
+    
+    static final String mail_footer = "If you think you have "
+            + "received this email by accident, please"
+            + " contact the aministrator (admin@hardosoft.com)";
 
-    static public boolean send(String from, String to, String subject, String data) {
-        
-        final int SMTP_PORT = Integer.parseInt(conf.getProperty("port"));
-        final String localhostName = conf.getProperty("localhostName");
-        final String mailhost = conf.getProperty("mailhost");
+    static public boolean send(List<String> recipients, String subject, String data) {
 
-        
-        try
-        {
+
+        try {
             Socket smtpPipe = new Socket(mailhost, SMTP_PORT);
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(smtpPipe.getInputStream()));
             PrintWriter out = new PrintWriter(
                     new OutputStreamWriter(smtpPipe.getOutputStream()), true);
-            in.readLine();
-            out.println("HELO " + localhostName);
-            if (!okReturned(in.readLine())) {
-                return false;
-            }
-            out.println("MAIL FROM:<" + from + ">");
-            if (!okReturned(in.readLine())) {
-                return false;
-            }
-            out.println("RCPT TO:<" + to + ">");
-            if (!okReturned(in.readLine())) {
-                return false;
-            }
-            out.println("DATA");
-            out.println("Subject:" + subject);
-            out.println(data);
-            out.println(".");
-            in.readLine();
-            if (!okReturned(in.readLine())) {
-                return false;
+            for (String recipient : recipients) {
+                in.readLine();
+                out.println("HELO " + localhostName);
+                if (!okReturned(in.readLine())) {
+                    return false;
+                }
+                out.println("MAIL FROM:<" + sender + ">");
+                if (!okReturned(in.readLine())) {
+                    return false;
+                }
+                out.println("RCPT TO:<" + recipient + ">");
+                if (!okReturned(in.readLine())) {
+                    return false;
+                }
+                out.println("DATA");
+                out.println("Subject:" + subject);
+                out.println(data);
+                out.println(".");
+                in.readLine();
+                if (!okReturned(in.readLine())) {
+                    return false;
+                }
             }
             out.println("QUIT");
-            
+
         } catch (IOException ex) {
-            System.err.println("Unable to connect to the SMTP server");
+            System.err.println("Unable to connect to the SMTP server. "
+                    + "Check mail server configuration");
             return false;
         }
         return true;
