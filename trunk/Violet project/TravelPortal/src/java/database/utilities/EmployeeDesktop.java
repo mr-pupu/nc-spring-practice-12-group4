7eb3eb47370aa2d9bcc35245205390839e75a52a
jiallombardo <jiallombardo@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-05-16 16:37:16 +0000

    Fixed Pablo's misunderstanding of count(*) aggregate function.

--- trunk/Violet project/TravelPortal/src/java/database/utilities/EmployeeDesktop.java
+++ trunk/Violet project/TravelPortal/src/java/database/utilities/EmployeeDesktop.java
@@ -1,6 +1,5 @@
 package database.utilities;
 
-import database.mapping.Trf;
 import database.mapping.Trfstate;
 import java.math.BigDecimal;
 import java.text.DateFormat;
@@ -11,46 +10,39 @@ import org.hibernate.Session;
 
 public class EmployeeDesktop {
 
-    
     /**
      * @author Allan
      * @param login
-     * @return the total number of "in progress" trfs of current user
-     * slow but working
+     * @return the total number of "in progress" trfs of current user slow but
+     * working
      */
-    public static long countEnteringRejectedTRF(String login){
+    public static long countEnteringRejectedTRF(String login) {
         String prepared_statement = "select count(*) "
                 + "from (select rownum r, employee_trfs.* "
                 + "from employee_trfs "
                 + "where (cur_state=1 or cur_state=0) and login=:login) ";
 
         Session s = HibernateUtil.getSession();
-        long res = Long.parseLong(s.createSQLQuery(prepared_statement).setString("login", login)
-                .list().get(0).toString());
+        long res = Long.parseLong(s.createSQLQuery(prepared_statement).setString("login", login).list().get(0).toString());
         return res;
     }
-    
-    
+
     //TRFs, the author of which is the given employee,
     //and state is Entering or Rejected
     public static String[][] EnteringRejectedTRF(String login, int page, int rows) {
         Session s = HibernateUtil.getSession();
-        int from = (page-1)*rows;
-        int to = from+rows;
+        int from = (page - 1) * rows;
+        int to = from + rows;
         String prepared_statement = "select id ,city_name, country_name, begin_date, end_date, cur_state, commentary "
                 + "from (select rownum r, employee_trfs.* "
                 + "from employee_trfs "
                 + "where (cur_state=1 or cur_state=0) and login=:login) "
                 + "where r> :from and r<= :to";
 
-        List resq = s.createSQLQuery(prepared_statement)
-                .setInteger("from", from)
-                .setInteger("to", to)
-                .setString("login", login)
-                .list();
-        
-                DateFormat reportDateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                
+        List resq = s.createSQLQuery(prepared_statement).setInteger("from", from).setInteger("to", to).setString("login", login).list();
+
+        DateFormat reportDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
         String[][] res = new String[resq.size()][7];
         for (int i = 0; i < resq.size(); i++) {
             BigDecimal trf_id = (BigDecimal) ((Object[]) resq.get(i))[0];
@@ -73,36 +65,31 @@ public class EmployeeDesktop {
         return res;
     }
 
-    public static long countAllEmpsTRFS(String login){
+    public static long countAllEmpsTRFS(String login) {
         String prepared_statement = "select count(*) "
                 + "from employee_trfs "
                 + "where login=:login ";
-        
+
         Session s = HibernateUtil.getSession();
-        long res = Long.parseLong(s.createSQLQuery(prepared_statement)
-                .setString("login", login).list().get(0).toString());
+        long res = Long.parseLong(s.createSQLQuery(prepared_statement).setString("login", login).list().get(0).toString());
         return res;
     }
 
     //TRFs, which are created by given employee
-      public static String[][] allEmpsTRFs(String login, int page, int rows) {
+    public static String[][] allEmpsTRFs(String login, int page, int rows) {
         Session s = HibernateUtil.getSession();
-        int from = (page-1)*rows;
-        int to = from+rows;
+        int from = (page - 1) * rows;
+        int to = from + rows;
         String prepared_statement = "select id ,city_name, country_name, begin_date, end_date, cur_state, commentary "
                 + "from (select rownum r, employee_trfs.* "
                 + "from employee_trfs "
                 + "where login=:login) "
                 + "where r> :from and r<= :to";
 
-        List resq = s.createSQLQuery(prepared_statement)
-                .setInteger("from", from)
-                .setInteger("to", to)
-                .setString("login", login)
-                .list();
-        
+        List resq = s.createSQLQuery(prepared_statement).setInteger("from", from).setInteger("to", to).setString("login", login).list();
+
         DateFormat reportDateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                
+
         String[][] res = new String[resq.size()][7];
         for (int i = 0; i < resq.size(); i++) {
             BigDecimal trf_id = (BigDecimal) ((Object[]) resq.get(i))[0];
@@ -124,14 +111,15 @@ public class EmployeeDesktop {
 
         return res;
     }
-      public static int HistoryCount(int id) {
+
+    public static BigDecimal HistoryCount(int id) {
         String pps = "SELECT COUNT(*) "
                 + "FROM trfstate "
                 + "WHERE trf_id=:id";
-        return  HibernateUtil.getSession().createSQLQuery(pps).setInteger("id", id).list().size();
+        return (BigDecimal)HibernateUtil.getSession().createSQLQuery(pps).setInteger("id", id).list().get(0);
     }
 
- public static List<Trfstate> HistoryPaged(int id, int page, int rows) {
+    public static List<Trfstate> HistoryPaged(int id, int page, int rows) {
         //prepare paging
         int start = (page - 1) * rows;
         int finish = start + rows;
@@ -141,7 +129,6 @@ public class EmployeeDesktop {
                 + "WHERE trf_id = :id) "
                 + "WHERE r>:start AND r<:finish";
         return (List<Trfstate>) HibernateUtil.getSession().createSQLQuery(pps).
-                addEntity(Trfstate.class).setInteger("id", id).setInteger("start", start)
-                .setInteger("finish", finish).list();
+                addEntity(Trfstate.class).setInteger("id", id).setInteger("start", start).setInteger("finish", finish).list();
     }
 }
