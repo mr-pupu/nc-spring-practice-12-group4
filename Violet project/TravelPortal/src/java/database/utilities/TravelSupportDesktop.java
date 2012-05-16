bf973d1f8ae3b227f05de3d48dc8029691d14915
Dudinskyj@gmail.com <Dudinskyj@gmail.com@8ee38f03-3965-bd2b-cafa-4c3929135087>
2012-05-16 14:24:36 +0000

    Update "All trfs table"

--- Violet project/TravelPortal/src/java/database/utilities/TravelSupportDesktop.java
+++ Violet project/TravelPortal/src/java/database/utilities/TravelSupportDesktop.java
@@ -112,7 +112,7 @@ public class TravelSupportDesktop {
 
         String prepared_statement = "select id, first_name, second_name,"
                 + "  dest_city, dest_country, begin_date,end_date, cur_state, commentary"
-                + "  from (select rownum r, trfs_report.*,  tso.commentary"
+                + "  from (select rownum r, trfs_report.* "
                 + "  from trfs_report JOIN trf_state_office tso ON tso.id=trfs_report.id "
                 + "    WHERE extract (month from maxdate)=(select to_char(sysdate,'mm') from dual)"
                 + "AND dep_name=:department "
@@ -136,7 +136,7 @@ public class TravelSupportDesktop {
     public static String[][] TrfSameCountryFilterByDate(Integer id, Date begin_date, Date end_date, int page, int rows) {
         String prepared_statement = "select id, first_name, second_name, "
                 + " dest_city, dest_country, begin_date,end_date, cur_state, commentary"
-                + " from (select rownum r, trfs_report.* "
+                + "  from (select rownum r, trfs_report.* "
                 + " from trfs_report "
                 + "    WHERE begin_date >:begin_date AND end_date<:end_date "
                 + "    AND office_country="
